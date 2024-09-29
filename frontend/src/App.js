d7f220f0a902777e3f377d6d1e3ce36259ebbfc1
kernelCruncher <43421475+kernelCruncher@users.noreply.github.com>
2024-09-29 19:34:19 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,7 +34,7 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h1>React and flask</h1>
+				<h1>Multi-fidelity Bayesian Optimization</h1>
 				{/* Calling a data from setdata for showing */}
 				<p>{data.name}</p>
 				<p>{data.age}</p>
